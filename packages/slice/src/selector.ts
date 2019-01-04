20039e779d44f9abf9766b225187f87ab90d08c2
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-04 13:24:29 +0100

    updated selector typing to handle '' as argument, added checks so informative Errors are throw when called with bad arguments

--- packages/slice/src/selector.ts
+++ packages/slice/src/selector.ts
@@ -1,24 +1,34 @@
-interface Hash {
-  [key: string]: any;
-}
+import { AnyState } from './slice';
 
-export function createSelector<State extends Hash, SliceState>(
-  slice: string,
-): (state: State) => SliceState {
-  if (!slice) {
-    return (state: State) => <any>state;
+export function createSelector<
+  State extends AnyState = AnyState,
+  SliceState = any
+>(slice: ''): (state: AnyState) => SliceState;
+
+export function createSelector<
+  State extends AnyState = AnyState,
+  SliceState = any
+>(slice: keyof State): (state: State) => SliceState;
+
+export function createSelector<
+  State extends AnyState = AnyState,
+  SliceState = any
+>(slice: keyof State): (state: State) => SliceState {
+  if (
+    !(
+      typeof slice === 'string' ||
+      typeof slice === 'number' ||
+      typeof slice === 'symbol'
+    )
+  ) {
+    throw new Error('slice argument must be a string or number or symbol');
   }
-  return (state: State) => state[slice];
-}
-export function createSelectorAlt<State extends Hash, SliceState>(
-  slice: string,
-): (state: State) => SliceState {
-  if (!slice) {
+  if (slice == null || slice === '') {
     return (state: State) => <any>state;
   }
   return (state: State) => {
     if (!state.hasOwnProperty(slice)) {
-      throw new Error(`${slice} was not found in the given State,
+      throw new Error(`${String(slice)} was not found in the given State,
       This selector was either called with a bad state argument or
       an incorrect slice name was given when instantiating the parent reducer,
       check for spelling errors`);
@@ -26,45 +36,67 @@ export function createSelectorAlt<State extends Hash, SliceState>(
     return state[slice];
   };
 }
-export function createSubSelector<State extends Hash, SliceState>(
+export function createSubSelector<
+  State extends AnyState = AnyState,
+  SliceState extends AnyState = AnyState
+>(slice: keyof State | '', subSlice: ''): never;
+export function createSubSelector<
+  State extends AnyState = AnyState,
+  SliceState extends AnyState = AnyState
+>(
+  slice: '',
+  subSlice: keyof SliceState,
+): (state: AnyState) => SliceState[keyof SliceState];
+export function createSubSelector<
+  State extends AnyState = AnyState,
+  SliceState extends AnyState = AnyState
+>(
+  slice: keyof State,
+  subSlice: keyof SliceState,
+): (state: State) => SliceState[keyof SliceState];
+export function createSubSelector<
+  State extends AnyState = AnyState,
+  SliceState extends AnyState = AnyState
+>(
   slice: keyof State,
   subSlice: keyof SliceState,
 ): (state: State) => SliceState[keyof SliceState] {
+  if (
+    subSlice == null ||
+    subSlice === '' ||
+    !(
+      typeof slice === 'string' ||
+      typeof slice === 'number' ||
+      typeof slice === 'symbol'
+    ) ||
+    !(
+      typeof subSlice === 'string' ||
+      typeof subSlice === 'number' ||
+      typeof subSlice === 'symbol'
+    )
+  ) {
+    throw new Error(
+      'SubSlice must not be blank, and slice/subSlice must be a string or number or symbol',
+    );
+  }
   if (!slice) {
     return (state: State) => state[subSlice as keyof State];
   }
   return (state: State) => {
     if (!state.hasOwnProperty(slice)) {
-      throw new Error(`${slice} was not found in the given State,
+      throw new Error(`${String(slice)} was not found in the given State,
       This selector was either called with a bad state argument or
       an incorrect slice name was given when instantiating the parent reducer,
       check for spelling errors`);
     }
+    if (!state[slice].hasOwnProperty(subSlice)) {
+      throw new Error(`${String(
+        subSlice,
+      )} was not found in the given State[${String(slice)}] slice,
+      This selector was either called with a bad state argument or
+      an incorrect subSlice name was given when instantiating the parent reducer,
+      check for spelling errors`);
+    }
     return (<SliceState>(<unknown>state[slice]))[subSlice];
   };
-}
-
-export function createSelectorName(slice: string) {
-  if (!slice) {
-    return 'getState';
-  }
-
-  return camelize(`get ${slice}`);
-}
-export function createSubSelectorName(slice: string, subSlice: string) {
-  if (!slice) {
-    return camelize(`get ${subSlice}`);
-  }
-
-  return camelize(`get ${slice} ${subSlice}`);
-}
-
-// https://stackoverflow.com/a/2970667/1713216
-function camelize(str: string) {
-  return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, (letter, index) => {
-      return index == 0 ? letter.toLowerCase() : letter.toUpperCase();
-    })
-    .replace(/\s+/g, '')
-    .replace(/[-_]/g, '');
 }
--- packages/slice/src/tests/selector.test.ts
+++ packages/slice/src/tests/selector.test.ts
@@ -1,20 +1,85 @@
-import { createSelectorName, createSubSelectorName } from '../selector';
+import { createSelector, createSubSelector } from '../selector';
 
-describe('createSelectorName', () => {
-  it('should convert to camel case', () => {
-    expect(createSelectorName('some')).toEqual('getSome');
-    expect(createSelectorName('someThing')).toEqual('getSomeThing');
-    expect(createSelectorName('some-thing')).toEqual('getSomeThing');
+describe('createSelector', () => {
+  const state = {
+    form: {
+      name: 'John',
+      surname: 'Doe',
+      middlename: 'Wayne',
+    },
+    auth: {
+      idToken: 'a random token',
+      userId: 'a user id',
+    },
+  };
+  const select = createSelector('');
+  const selectForm = createSelector('form');
+  const selectAuth = createSelector('auth');
+  const selectInvalid = createSelector('invalid');
+  it('should select the correct slice', () => {
+    expect(select(state)).toEqual(state);
+    expect(selectForm(state)).toEqual(state.form);
+    expect(selectAuth(state)).toEqual(state.auth);
+  });
+  it('throws when instantiated with an invalid slice', () => {
+    expect(() => {
+      createSelector(<any>{ slice: 'slice' });
+    }).toThrow(/must be a string or number or symbol/);
+    expect(() => {
+      createSelector(<any>['slice']);
+    }).toThrow(/must be a string or number or symbol/);
+  });
+  it('throws when instantiated with an slice not present in the state', () => {
+    expect(() => {
+      selectInvalid(<any>state);
+    }).toThrow(/was not found in the given State/);
+  });
+  it('throws when called with an invalid state', () => {
+    expect(() => {
+      selectAuth(<any>{ form: {} });
+    }).toThrow(/ was not found in the given State/);
   });
 });
-describe('createSubSelectorName', () => {
-  it('should convert to camel case', () => {
-    expect(createSubSelectorName('some', 'thing')).toEqual('getSomeThing');
-    expect(createSubSelectorName('someThing', 'else')).toEqual(
-      'getSomeThingElse',
-    );
-    expect(createSubSelectorName('some-thing', 'else-now')).toEqual(
-      'getSomeThingElseNow',
-    );
+describe('createSubSelector', () => {
+  const state = {
+    form: {
+      name: 'John',
+      surname: 'Doe',
+      middlename: 'Wayne',
+    },
+    auth: {
+      idToken: 'a random token',
+      userId: 'a user id',
+    },
+  };
+  const selectFormName = createSubSelector('form', 'name');
+  const selectFormSurname = createSubSelector('form', 'surname');
+  const selectFormMiddlename = createSubSelector('form', 'middlename');
+  const selectAuthIdToken = createSubSelector('auth', 'idToken');
+  const selectAuthUserId = createSubSelector('auth', 'userId');
+
+  it('should throw when called with blank or non-string|number or slice subSlice', () => {
+    expect(() => {
+      createSubSelector('', '');
+    }).toThrow(/SubSlice must not be blank/);
+    expect(() => {
+      createSubSelector(<any>{ gf: 'dfdf' }, '');
+    }).toThrow(/must be a string or number or symbol/);
+    expect(() => {
+      createSubSelector(<any>{ slice: 'slice' }, 'subSlice');
+    }).toThrow(/must be a string or number or symbol/);
+    expect(() => {
+      createSubSelector(<any>['slice'], '');
+    }).toThrow(/must be a string or number or symbol/);
+    expect(() => {
+      createSubSelector(<any>['slice'], 'subSlice');
+    }).toThrow(/must be a string or number or symbol/);
+  });
+  it('should select the correct subSlice', () => {
+    expect(selectFormName(state)).toEqual('John');
+    expect(selectFormMiddlename(state)).toEqual('Wayne');
+    expect(selectFormSurname(state)).toEqual('Doe');
+    expect(selectAuthIdToken(state)).toEqual('a random token');
+    expect(selectAuthUserId(state)).toEqual('a user id');
   });
 });
