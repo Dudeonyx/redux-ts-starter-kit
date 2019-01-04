ad2951499365a4c3de741d9a3c519dd9f2f692f9
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-04 14:02:39 +0100

    null no longer a non serializable value

--- packages/core/src/isPlainObject.ts
+++ packages/core/src/isPlainObject.ts
@@ -2,8 +2,11 @@
  * @param {any} obj The object to inspect.
  * @returns {boolean} True if the argument appears to be a plain object.
  */
-export default function isPlainObject(obj?: any): obj is { [x: string]: any } {
-  if (typeof obj !== 'object' || obj === null) return false;
+export default function isPlainObject(obj?: any): obj is object {
+  if (typeof obj !== 'object') return false;
+  if (obj === null) {
+    return true;
+  }
 
   let proto = obj;
   while (Object.getPrototypeOf(proto) !== null) {
