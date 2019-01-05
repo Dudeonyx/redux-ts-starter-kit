583fde9c702b3aa42a0b7057c790fc2bb5e1bc5e
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-05 16:58:50 +0100

    add sourceMaps and rollup-plugin-terser for minification

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,20 +1,28 @@
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
+import { terser } from 'rollup-plugin-terser';
 export default {
   input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
       format: 'cjs',
+      sourcemap: true,
     },
     {
       file: pkg.module,
       format: 'es',
+      sourcemap: true,
     },
   ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
-  plugins: [typescript()],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    }),
+    terser(),
+  ],
 };
