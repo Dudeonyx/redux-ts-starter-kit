bdf061b0478c2be8f7e90a3c81dc422bbe0aea91
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-05 22:17:12 +0100

    exposed even more types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,42 @@
 export { configureStore, getDefaultMiddleware } from './configureStore';
-export * from '@redux-ts-starter-kit/slice';
-export * from 'reselect';
+export {
+  createAction,
+  createReducer,
+  createSlice,
+  getActionType,
+  Action,
+  AnyAction,
+  AnyState,
+  CreateReducer,
+  NoEmptyArray,
+  ReduceM,
+  Slice,
+  createNextState,
+} from '@redux-ts-starter-kit/slice';
+export {
+  OutputParametricSelector,
+  OutputSelector,
+  ParametricSelector,
+  Selector,
+  createSelector,
+  createSelectorCreator,
+  createStructuredSelector,
+  defaultMemoize,
+} from 'reselect';
+export {
+  ActionCreator,
+  DeepPartial,
+  Dispatch,
+  ActionCreatorsMapObject,
+  Middleware,
+  MiddlewareAPI,
+  Reducer,
+  ReducersMapObject,
+  Store,
+  StoreEnhancer,
+  StoreCreator,
+  Unsubscribe,
+  StoreEnhancerStoreCreator,
+} from 'redux';
+export { EnhancerOptions } from 'redux-devtools-extension';
+export { ThunkAction, ThunkDispatch, ThunkMiddleware } from 'redux-thunk';
