1fe8eaf30be745765731e01f10fee9453f823611
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-06 15:00:47 +0100

    fixed issue with exported Dispatch type

--- packages/slice/src/index.ts
+++ packages/slice/src/index.ts
@@ -2,5 +2,5 @@ export * from './action';
 export * from './reducer';
 import createNextState from 'immer';
 export { createNextState };
-export * from './types';
+export { Dispatch } from 'redux';
 export * from './slice';
