d5cf13ec4d935801c682a3aef2d168b74d6b57b2
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-01-11 22:44:56 +0100

    Wrapped `slice` and `action` in String constructor to prevent possible runtime errors

--- packages/slice/src/actionTypeBuilder.ts
+++ packages/slice/src/actionTypeBuilder.ts
@@ -1,4 +1,9 @@
 const allCapsSnakeCase = (string: string) =>
   string.replace(/([a-z])([A-Z])/g, '$1_$2').toUpperCase();
-export const actionTypeBuilder = (slice: string) => (action: string) =>
-  slice ? `${slice}/${allCapsSnakeCase(action)}` : allCapsSnakeCase(action);
+export const actionTypeBuilder = (slice: string | number | symbol) => {
+  return (action: string | number | symbol) => {
+    return slice != null && slice !== ''
+      ? `${String(slice)}/${allCapsSnakeCase(String(action))}`
+      : allCapsSnakeCase(String(action));
+  };
+};
