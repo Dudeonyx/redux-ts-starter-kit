b1a6ee13c6ade0aaeab7d7af51c61064e4db032e
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2019-05-03 01:16:32 +0100

    removed reselect import

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,7 +6,7 @@ export {
   createSlice,
   getActionType,
 } from '@redux-ts-starter-kit/slice';
-export {createSelector,createSelectorCreator,createStructuredSelector,defaultMemoize} from 'reselect';
+export { default as memoize } from 'memoize-state';
 export { EnhancerOptions } from 'redux-devtools-extension';
 export { ThunkDispatch, ThunkMiddleware, ThunkAction } from 'redux-thunk';
 export { bindActionCreators, Dispatch, Action, AnyAction } from 'redux';
