fe30c8b687ae1ab4c08eaf01c1d876f79869300b
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2021-07-31 21:56:34 +0100

    re-export reselect, createSelect uses memoize-state

--- packages/slice/src/index.ts
+++ packages/slice/src/index.ts
@@ -1,7 +1,14 @@
+import memoize from 'memoize-state';
+import * as reselect from 'reselect';
+
+export * from 'reselect';
 export * from './action';
 export * from './reducer';
 export { produce as createNextState } from 'immer';
 export { default as memoize } from 'memoize-state';
 export * from './slice';
 export * from './types';
+
+export const createSelector = reselect.createSelectorCreator(memoize);
+
 // export { makeTypeSafeSelector } from './selector';
